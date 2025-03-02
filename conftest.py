ba97b162011db463c7600a6391c3a5370a2e85a5
Anna Santalova <asantalova@deloitte.gr>
2025-03-03 00:04:24 +0200

    options

--- conftest.py
+++ conftest.py
@@ -22,6 +22,7 @@ def browser():
     chrome_options.add_argument("--disable-gpu") # for CI/CD
     chrome_options.add_argument("--no sandbox") # for CI/CD
     chrome_options.add_argument("disable-dev-shm-usage")
+    chrome_options.binary_location = "/usr/bin/google-chrome"
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.implicitly_wait(10)
