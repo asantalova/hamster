e06e64f157151743ba4d1209c185a91905823860
Anna Santalova <asantalova@deloitte.gr>
2025-03-02 15:05:38 +0200

    driver manager

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 #Store test results globally
 test_results = []
@@ -21,7 +22,7 @@ def browser():
     chrome_options.add_argument("--disable-gpu") # for CI/CD
     chrome_options.add_argument("--no sandbox") # for CI/CD
     chrome_options.add_argument("disable-dev-shm-usage")
-    service = Service('/harness/chromedriver')
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.implicitly_wait(10)
     yield driver
