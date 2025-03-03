9782bd2b93a5850adea0cf566504a724a4dc11e0
Anna Santalova <asantalova@deloitte.gr>
2025-03-03 14:49:37 +0200

    options

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import time
 import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.options import Service
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 #Store test results globally
